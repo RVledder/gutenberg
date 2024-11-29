34c63c78bcee42b21985c747bdbc7a6e45a1b9cb
Ella <ella@vandurpe.com>
2024-11-29 20:35:41 +0000

    Improve performance

--- packages/block-editor/src/components/block-list/use-block-props/use-firefox-draggable-compatibility.js
+++ packages/block-editor/src/components/block-list/use-block-props/use-firefox-draggable-compatibility.js
@@ -3,6 +3,48 @@
  */
 import { useRefEffect } from '@wordpress/compose';
 
+class NodeSet extends Set {
+	constructor() {
+		super();
+		this.documents = new Set();
+	}
+	add( node ) {
+		const { ownerDocument } = node;
+		if ( ! this.documents.has( ownerDocument ) ) {
+			ownerDocument.addEventListener( 'pointerdown', down );
+			this.documents.add( ownerDocument );
+		}
+		return super.add( node );
+	}
+}
+
+const nodes = new NodeSet();
+
+function down( event ) {
+	if ( event.target.isContentEditable ) {
+		// Whenever an editable element is clicked, check which draggable
+		// blocks contain this element, and temporarily disable draggability.
+		for ( const node of nodes ) {
+			if (
+				node.getAttribute( 'draggable' ) === 'true' &&
+				node.contains( event.target )
+			) {
+				node.setAttribute( 'draggable', 'false' );
+				node.setAttribute( 'data-draggable', 'true' );
+			}
+		}
+	} else {
+		// Whenever a non-editable element is clicked, re-enable draggability
+		// for any blocks that were previously disabled.
+		for ( const node of nodes ) {
+			if ( node.getAttribute( 'data-draggable' ) === 'true' ) {
+				node.setAttribute( 'draggable', 'true' );
+				node.removeAttribute( 'data-draggable' );
+			}
+		}
+	}
+}
+
 /**
  * In Firefox, the `draggable` and `contenteditable` attributes don't play well
  * together. When `contenteditable` is within a `draggable` element, selection
@@ -13,24 +55,9 @@ import { useRefEffect } from '@wordpress/compose';
  */
 export function useFirefoxDraggableCompatibility() {
 	return useRefEffect( ( node ) => {
-		function onDown( event ) {
-			if ( node.draggable === true ) {
-				if ( event.target.isContentEditable ) {
-					node.draggable = false;
-					node.setAttribute( 'data-draggable', 'true' );
-				}
-			} else if (
-				! event.target.isContentEditable &&
-				node.getAttribute( 'data-draggable' ) === 'true'
-			) {
-				node.draggable = true;
-				node.removeAttribute( 'data-draggable' );
-			}
-		}
-		const { ownerDocument } = node;
-		ownerDocument.addEventListener( 'pointerdown', onDown );
+		nodes.add( node );
 		return () => {
-			ownerDocument.removeEventListener( 'pointerdown', onDown );
+			nodes.delete( node );
 		};
 	}, [] );
 }
